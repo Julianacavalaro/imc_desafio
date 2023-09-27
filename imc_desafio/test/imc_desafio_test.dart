ce9262b454f459ad94c189ababe2641b0991f474
Juliana Cavalaro de Oliveira <juliana.cavalaro@hotmail.com>
2023-09-27 17:17:58 -0400

    Teste unitario

--- imc_desafio/test/imc_desafio_test.dart
+++ imc_desafio/test/imc_desafio_test.dart
@@ -1,8 +1,14 @@
-import 'package:imc_desafio/imc_desafio.dart';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Teste com valores válidos', () {
+    final input = 'Alice\n70\n1.75\n';
+    final expectedOutput = 'Seu IMC e igual a: 22.86\nSaudavel\n';
+
+    final result = runCalculaIMC(input);
+    expect(result, equals(expectedOutput));
   });
 }
